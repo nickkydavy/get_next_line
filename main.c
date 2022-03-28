2f1e6ab86eba6e582e69c8f9fcff5f970869f125
Panu Nimwattana <p.nimwattana@gmail.com>
2022-03-28 13:25:59 +0700

    idea 4 edit 2

--- main.c
+++ main.c
@@ -1,7 +1,26 @@
 #include "get_next_line.h"
+#include <fcntl.h>
 
 int	main(void)
 {
-	int	fd = open("41_no_nl", O_RDONLY);
+	printf("passthis");
+	int	fd = open("multiple_line_with_nl", O_RDONLY);
+	if (!fd)
+	{
+		printf("error");
+		return (0);
+	}
+	char	*str;
+	// int		i = 1;
+	printf("passthis");
+	str = get_next_line(fd);
+	/*while ((str = get_next_line(fd)) != 0)
+	{
+		printf("%d\t%ld\n", i, ft_strlen(str));
+		printf("%s", str);
+		printf("\n");
+	}*/
+	free(str);
+	close(fd);
 	return (0);
 }
